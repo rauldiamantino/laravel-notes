4e217fffb931b353c5495d2e1dfd778b5d3b30a0
Raul Diamantino <rauldiamantino25@gmail.com>
2025-11-12 01:11:48 -0300

    008

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/login', [AuthController::class, 'login']);
+Route::get('/logout', [AuthController::class, 'logout']);
