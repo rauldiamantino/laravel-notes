f043a8e8c49294721ffa495b90f82f1730a7db35
Raul Diamantino <rauldiamantino25@gmail.com>
2025-11-12 13:21:04 -0300

    014

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,9 +13,22 @@ class AuthController extends Controller
 
     public function loginSubmit(Request $request)
     {
-        echo $request->input('text_username');
-        echo '<br>';
-        echo $request->input('text_password');
+        // Form validation
+        $request->validate(
+            // rules
+            [
+                'text_username' => 'required|email',
+                'text_password' => 'required|min:6|max:16',
+            ],
+            // error messages
+            [
+                'text_username.required' => 'O username é obrigatório',
+                'text_username.email' => 'O username deve ser um email válido',
+                'text_password.required' => 'A senha é obrigatória',
+                'text_password.min' => 'A senha deve ter pelo menos :min caracteres',
+                'text_password.max' => 'A senha deve ter pelo menos :max caracteres',
+            ],
+        );
     }
 
     public function logout()
