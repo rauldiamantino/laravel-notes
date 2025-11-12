e1d6c4feda46e2baf5315d894fa718a94e0cecc7
Raul Diamantino <rauldiamantino25@gmail.com>
2025-11-12 01:28:21 -0300

    009

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,7 +8,14 @@ class AuthController extends Controller
 {
     public function login()
     {
-        echo 'login';
+        return view('login');
+    }
+
+    public function loginSubmit(Request $request)
+    {
+        echo $request->input('text_username');
+        echo '<br>';
+        echo $request->input('text_password');
     }
 
     public function logout()
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,5 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/login', [AuthController::class, 'login']);
+Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
 Route::get('/logout', [AuthController::class, 'logout']);
