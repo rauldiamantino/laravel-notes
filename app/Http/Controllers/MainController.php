53a74c9283ed3b577e4c94bea7c6c7875a01a227
Raul Diamantino <rauldiamantino25@gmail.com>
2025-11-13 21:32:07 -0300

    034

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -66,7 +66,12 @@ class MainController extends Controller
     public function editNote($id)
     {
         $id = Operations::decryptId($id);
-        echo 'Im editing Note with ID ' . $id;
+
+        // Load note
+        $note = Note::find($id);
+
+        // Show edit note view
+        return view('edit_note', ['note' => $note]);
     }
 
     public function deleteNote($id)
